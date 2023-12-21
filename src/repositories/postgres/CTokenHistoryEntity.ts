00e3b1ca1795d9e64b19165645805af1f00856b2
smaki <50903600+smit-akiyama@users.noreply.github.com>
2023-12-21 17:15:52 +0900

    Node.jsのバージョンアップ対応として修正

--- src/repositories/postgres/CTokenHistoryEntity.ts
+++ src/repositories/postgres/CTokenHistoryEntity.ts
@@ -25,23 +25,23 @@ export default class CTokenHistoryEntity extends BaseEntity {
 
     /** CTokenID */
     @Column({ type: 'bigint', nullable: false, name: 'ctoken_id' })
-    ctokenId: number;
+        ctokenId: number;
 
     /** CToken */
     @Column({ type: 'varchar', length: 255, nullable: false, name: 'ctoken' })
-    ctokenHistory: string = '';
+        ctokenHistory: string = '';
 
     /** CToken作成時間 */
     @Column({ type: 'timestamp without time zone', nullable: false, name: 'ctoken_create_at', onUpdate: 'now()' })
-    ctokenCreateAt: Date;
+        ctokenCreateAt: Date;
 
     /** 削除フラグ */
     @Column({ type: 'boolean', nullable: false, default: false, name: 'is_disabled' })
-    isDisabled: boolean = false;
+        isDisabled: boolean = false;
 
     /** 登録者 */
     @Column({ type: 'varchar', length: 255, nullable: false, name: 'created_by' })
-    createdBy: string = '';
+        createdBy: string = '';
 
     /** 登録日時 */
     @CreateDateColumn({ type: 'timestamp without time zone', nullable: false, name: 'created_at' })
@@ -49,7 +49,7 @@ export default class CTokenHistoryEntity extends BaseEntity {
 
     /** 更新者 */
     @Column({ type: 'varchar', length: 255, nullable: false, name: 'updated_by' })
-    updatedBy: string = '';
+        updatedBy: string = '';
 
     /** 更新日時 */
     @UpdateDateColumn({ type: 'timestamp without time zone', nullable: false, name: 'updated_at', onUpdate: 'now()' })
@@ -58,5 +58,5 @@ export default class CTokenHistoryEntity extends BaseEntity {
     /** CTokenテーブルのレコード */
     @ManyToOne(type => CTokenEntity, ctoken => ctoken.cmatrixs)
     @JoinColumn({ name: 'ctoken_id', referencedColumnName: 'id' })
-    ctoken: CTokenEntity;
+        ctoken: CTokenEntity;
 }
